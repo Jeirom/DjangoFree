4fe9d4972df08788065569b6c3fe84946fb9b35a
Daniil <jeiromkrizhanovskiyd@gmail.com>
2025-01-19 11:43:46 +0500

    Начал делать дз №2 по курсу "Джанго" Добавил модель(класс) категория и продукт. Создал суперпользователя, сделал миграции. Ну и настроички всякие

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Student(models.Model):
+    FIRST_YEAR = 'first'
+    SECOND_YEAR = 'second'
+    THIRD_YEAR = 'third'
+    FOURTH_YEAR = 'fourth'
+
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FIRST_YEAR, 'Первый курс'),
+        (SECOND_YEAR, 'Второй курс'),
+        (THIRD_YEAR, 'Третий курс'),
+        (FOURTH_YEAR, 'Четвертый курс'),
+    ]
+
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    year = models.CharField(
+        max_length=6,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FIRST_YEAR,
+        verbose_name='Курс'
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+        ordering = ['last_name']
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(max_length=150, verbose_name='Описание')
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(max_length=150, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
